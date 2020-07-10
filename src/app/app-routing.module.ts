1cb5e22a8124fde85e88bcfe565ac6db34662e8c
Kaleemullah Nizamani <nixamani5@gmail.com>
2020-07-10 21:51:54 +0500

    fixed route issues

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes/heroes.component';
-import { AuthGuard} from './shared/guards/auth.guard'
-import { LoggedInGuard} from './shared/guards/logged.in.guard'
+import { AuthGuard } from './shared/guards/auth.guard'
+import { LoggedInGuard } from './shared/guards/logged.in.guard'
 import { LoginComponent } from './shared/ngxs-store/authentication/login/login.component';
 
 const routes: Routes = [
 
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   {
-  path: 'login',
-  component: LoginComponent,
-  canActivate: [LoggedInGuard]
-},
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [LoggedInGuard]
+  },
 
   {
     path: 'heroes',
     component: HeroesComponent,
     canActivate: [AuthGuard]
   },
+  {
+    path: '**', component:
+      LoginComponent,
+    canActivate: [LoggedInGuard]
+  }
 ];
 
 @NgModule({
